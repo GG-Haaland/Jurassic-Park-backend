e37498ef9355b53cb0a2611f2bb2862be92c0ab1
tiffpereira <tiffanympereira@gmail.com>
2022-03-24 11:31:45 -0400

    updating files in seed

--- models/ride.js
+++ models/ride.js
@@ -5,7 +5,7 @@ const Ride = new Schema(
         id_number: {type: String, required: true},
         description: {type: String, required: true},
         url: {type: String, required: true},
-        min_height: {type: String, required: true},
+        min_height: {type: Boolean, required: true},
     },
 
     {timestamps: true}
